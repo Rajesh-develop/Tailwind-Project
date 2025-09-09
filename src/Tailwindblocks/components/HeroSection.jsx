406d112bf3f12937e5424c20af99c53c697caf17
Thoti Rajesh <rajeshthoti6@gmail.com>
2025-09-09 23:16:42 +0530

    public path changed

--- src/Tailwindblocks/components/HeroSection.jsx
+++ src/Tailwindblocks/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
       </div>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className="object-cover object-center rounded" alt="hero" src="public/images/assorted-colorful-ice-cream-scoops-toppings-blue-background-vibrant-assortment-sprinkles-strawberries-candy-371702184.webp"/>
+      <img className="object-cover object-center rounded" alt="hero" src="/images/assorted-colorful-ice-cream-scoops-toppings-blue-background-vibrant-assortment-sprinkles-strawberries-candy-371702184.webp"/>
     </div>
   </div>
 </section>
